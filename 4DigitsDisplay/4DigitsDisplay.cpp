c36e85605f5b05eef52e0307d89b71b3566246c0
Kraecktus <kraecktusmax@gmail.com>
2022-09-13 16:18:45 +0200

    Update 4DigitsDisplay.cpp

--- 4DigitsDisplay/4DigitsDisplay.cpp
+++ 4DigitsDisplay/4DigitsDisplay.cpp
@@ -2,9 +2,9 @@
 #include <Arduino.h>
 #include <TM1637Display.h>
 
-const int CLK = 4;
-const int DIO = 0;
-TM1637Display display(CLK, DIO);
+const int CLK = 4;                              // Set the CLK pin connection to the display
+const int DIO = 0;                              // Set the DIO pin connection to the display
+TM1637Display display(CLK, DIO);                // set up the 4-Digit Display.
 uint8_t Segments[4] = {0x00, 0x00, 0x00, 0x00}; // blank on start
 int Count[4] = {0, 0, 0, 0};
 
@@ -25,14 +25,19 @@ void setup()
 
 void loop()
 {
-    Count[0] += 1;
-    Count[1] += 1;
-    Count[2] += 1;
     Count[3] += 1;
+    for (int i = 3; i > 0; i--)
+    {
+        if (Count[i] == 9)
+        {
+            Count[i - 1] += 1;
+            Count[i] = 0;
+        }
+    }
     Segments[0] = ConvertNum(Count[0]);
     Segments[1] = ConvertNum(Count[1]);
     Segments[2] = ConvertNum(Count[2]);
     Segments[3] = ConvertNum(Count[3]);
     SetDisplay();
-    delay(250);
+    delay(100);
 }
